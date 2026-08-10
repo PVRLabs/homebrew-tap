2da2556804f780d9870215e04dd162418c484a2f
Ted Kupolov <kupolov@gmail.com>
2026-08-10 15:29:45 -0700

    statlite: update to 0.2.3

--- Formula/statlite.rb
+++ Formula/statlite.rb
@@ -1,29 +1,30 @@
 class Statlite < Formula
   desc "Tiny self-hosted metrics dashboard for small servers"
   homepage "https://github.com/PVRLabs/statlite"
+  version "0.2.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/PVRLabs/statlite/releases/download/v0.2.2/statlite_0.2.2_darwin_arm64.tar.gz"
-      sha256 "8f0a45902efc32fe954883e92e406ae30648bffd203409439391a6e0fe34d6be"
+      url "https://github.com/PVRLabs/statlite/releases/download/v#{version}/statlite_#{version}_darwin_arm64.tar.gz"
+      sha256 "e4cf35447dfffd3bc23d7b98c9f2dced1c738b1da6db98d25099ccb2f71c2467"
     end
 
     on_intel do
-      url "https://github.com/PVRLabs/statlite/releases/download/v0.2.2/statlite_0.2.2_darwin_amd64.tar.gz"
-      sha256 "c94f7f127709f6dda8d0ce436204f4f1fa63a931095fd96bddeebe4442a86b27"
+      url "https://github.com/PVRLabs/statlite/releases/download/v#{version}/statlite_#{version}_darwin_amd64.tar.gz"
+      sha256 "c5e2b14b2dc04d88f883554495e9530557130eb828923b968fe8a13798ee384e"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/PVRLabs/statlite/releases/download/v0.2.2/statlite_0.2.2_linux_arm64.tar.gz"
-      sha256 "7c0d24955c824576470a4b5ebc7d1f86f7d7784db7803d8bf84db43f65a73598"
+      url "https://github.com/PVRLabs/statlite/releases/download/v#{version}/statlite_#{version}_linux_arm64.tar.gz"
+      sha256 "e62227a023c152ba82772a899db4340dd3756b454d4bc6091d65c8b89dfe8081"
     end
 
     on_intel do
-      url "https://github.com/PVRLabs/statlite/releases/download/v0.2.2/statlite_0.2.2_linux_amd64.tar.gz"
-      sha256 "ed33a57447b0f0dd8ca423917e00838c9ec7e0a2aaf744f92e544a7d0902d04b"
+      url "https://github.com/PVRLabs/statlite/releases/download/v#{version}/statlite_#{version}_linux_amd64.tar.gz"
+      sha256 "d3e9cc7f92e59dfa715146dc49d0f5449134959d37170214e22e30d39cc40efb"
     end
   end
 
