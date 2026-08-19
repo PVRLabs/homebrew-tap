8aaf50fef8aa3e84be931754c2b20fe8456e98da
Ted Kupolov <kupolov@gmail.com>
2026-08-19 14:47:04 -0700

    statlite: update to 0.3.0

--- Formula/statlite.rb
+++ Formula/statlite.rb
@@ -1,30 +1,30 @@
 class Statlite < Formula
   desc "Tiny self-hosted metrics dashboard for small servers"
   homepage "https://github.com/PVRLabs/statlite"
-  version "0.2.3"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/PVRLabs/statlite/releases/download/v#{version}/statlite_#{version}_darwin_arm64.tar.gz"
-      sha256 "e4cf35447dfffd3bc23d7b98c9f2dced1c738b1da6db98d25099ccb2f71c2467"
+      sha256 "e09bfe8936db70be5394a26d0547ffadd332bda4a074fca9048c557ece02aaee"
     end
 
     on_intel do
       url "https://github.com/PVRLabs/statlite/releases/download/v#{version}/statlite_#{version}_darwin_amd64.tar.gz"
-      sha256 "c5e2b14b2dc04d88f883554495e9530557130eb828923b968fe8a13798ee384e"
+      sha256 "a436596cc98e989ba547d5de05876ec3d38a62a7a58183cd4dee7719476317f0"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/PVRLabs/statlite/releases/download/v#{version}/statlite_#{version}_linux_arm64.tar.gz"
-      sha256 "e62227a023c152ba82772a899db4340dd3756b454d4bc6091d65c8b89dfe8081"
+      sha256 "b0d262859118b610a6821442b3679907a6c6ef959fb65e3353e1b3bbde5cf968"
     end
 
     on_intel do
       url "https://github.com/PVRLabs/statlite/releases/download/v#{version}/statlite_#{version}_linux_amd64.tar.gz"
-      sha256 "d3e9cc7f92e59dfa715146dc49d0f5449134959d37170214e22e30d39cc40efb"
+      sha256 "b0c1bfeef082d31ecee0fa175d0314eb46a746ba1592aecf9027e827d6544ab9"
     end
   end
 
